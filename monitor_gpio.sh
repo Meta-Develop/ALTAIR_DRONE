bd4318797444302bed7a78befca9ac9890298408
Meta-Develop <rikuuyoo@gmail.com>
2025-12-22 13:22:13 +0900

    debug: Add monitor_gpio.sh safely

---
+++ monitor_gpio.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+GPIO=9
+if [ ! -d /sys/class/gpio/gpio$GPIO ]; then
+    echo "Exporting GPIO $GPIO"
+    echo $GPIO > /sys/class/gpio/export
+    sleep 1
+fi
+echo in > /sys/class/gpio/gpio$GPIO/direction
+
+echo "Monitoring GPIO $GPIO MISO Input..."
+while true; do
+  cat /sys/class/gpio/gpio$GPIO/value
+  sleep 0.1
+done
